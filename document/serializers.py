d6ff59fd593b96cfd97be6bb5d49a7b0057da602
Evgenii <evgeny.shevchenko@webware24.de>
2021-01-30 22:05:22 +0300

    add nlp res

---
+++ document/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from .models import *
+
+
+class NlpResultListSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = NlpResult
+        fields = ("document_id", "cve", "cwe", "software", "malware", "course_of_action", "intrusion_set",
+                 "threat_actor", "tool", "attack_pattern", "industry", "mitre_attack", "campaign", "org",
+                 "country", "city", "geolocation", "time_stamp", "ioc", "technique")
